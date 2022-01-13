4599ad8b0a06318076073feb51155fc64b752c3c
ychcg <69442638+ychcg@users.noreply.github.com>
2022-01-13 15:55:22 +0900

    excel download

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
-
+import xlsx from "xlsx";
 function App() {
+  const readUploadFile = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = xlsx.read(data);
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = JSON.stringify(xlsx.utils.sheet_to_json(worksheet));
+
+        const a = document.createElement("a");
+        const blob1 = new Blob([json], {
+          type: "application/json",
+        });
+        a.href = URL.createObjectURL(blob1);
+        a.download = "excel.json";
+        a.click();
+      };
+
+      reader.readAsArrayBuffer(e.target.files[0]);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Upload Excel File!</p>
-        <input type="file" onChange={() => {}} />
+        <input type="file" onChange={readUploadFile} />
       </header>
     </div>
   );
