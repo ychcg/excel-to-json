9e0a2759c99f70d4ac1345c89649a427fb6fa582
ychcg <69442638+ychcg@users.noreply.github.com>
2022-01-13 15:11:03 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Upload Excel File!</p>
+        <input type="file" onChange={() => {}} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
