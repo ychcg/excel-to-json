5d417bd5ef90d6aa8395e03f1d85fb9d72dcec28
ychcg <69442638+ychcg@users.noreply.github.com>
2022-01-13 16:41:39 +0900

    download 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { useState } from "react";
 import xlsx from "xlsx";
 function App() {
+  const [data, setData] = useState(null);
+
   const readUploadFile = (e) => {
     e.preventDefault();
-    if (e.target.files) {
+
+    if (e.target.files.length > 0) {
       const reader = new FileReader();
       reader.onload = (e) => {
         const data = e.target.result;
@@ -12,24 +16,67 @@ function App() {
         const worksheet = workbook.Sheets[sheetName];
         const json = JSON.stringify(xlsx.utils.sheet_to_json(worksheet));
 
-        const a = document.createElement("a");
-        const blob1 = new Blob([json], {
-          type: "application/json",
-        });
-        a.href = URL.createObjectURL(blob1);
-        a.download = "excel.json";
-        a.click();
+        setData(json);
       };
 
       reader.readAsArrayBuffer(e.target.files[0]);
+    } else {
+      setData(null);
     }
+  };
+
+  const downloadChCsm = () => {
+    const parsedData = JSON.parse(data);
+    const convertedData = {};
+
+    parsedData.forEach((d, i) => {
+      const { email, firstHalf, secondHalf, selfGrade, processType } = d;
+      const FIRST = "상반기만";
+      const SECOND = "하반기만";
+
+      let type = {};
+
+      if (processType.includes(FIRST)) {
+        type.useOnlyFirstHalf = true;
+      } else if (processType.includes(SECOND)) {
+        type.useOnlySecondHalf = true;
+      }
+
+      convertedData[email] = {
+        id: i + 1,
+        firstHalf,
+        secondHalf,
+        selfGrade,
+        ...type,
+      };
+    });
+
+    generateBlob(JSON.stringify(convertedData));
+  };
+
+  const generateBlob = (d) => {
+    const a = document.createElement("a");
+    const blob1 = new Blob([d], {
+      type: "application/json",
+    });
+    a.href = URL.createObjectURL(blob1);
+    a.download = "excel.json";
+    a.click();
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>Upload Excel File!</p>
-        <input type="file" onChange={readUploadFile} />
+        <p>Upload Excel File</p>
+        <div>
+          <input type="file" onChange={readUploadFile} />
+        </div>
+        {data && (
+          <div style={{ marginTop: 20 }}>
+            <p>click if you have permission !</p>
+            <button onClick={downloadChCsm}>FOR CH-CSM BUTTON</button>
+          </div>
+        )}
       </header>
     </div>
   );
